a6912cdec27a4ded3b18fc6dbc92b6a97f3c1f74
Scott Jonker <kaosfanatic@ymail.com>
2017-12-01 16:57:14 -0600

    "Added .gitignore"

--- knight.cpp
+++ knight.cpp
@@ -6,18 +6,7 @@
 using namespace std;
 
 
-/* TODO LIST
 
-TODO: Add Class (Square)
-	Elements: Key, Link pointers (x8)
-	Methods: Data returns, default constructor, Set link
-	
-TODO: Function to initialize a GRID of SQUARE objects
-TODO: Function to set links for GRID
-
-TODO: Function to return least moves, utilizing a breadth first search and a boolean traversal array
-
-END LIST */
 
 class Square{
 	 
